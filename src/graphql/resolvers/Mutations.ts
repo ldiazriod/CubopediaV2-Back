4c72a5c476b4863c0c3aa663db01015a1b903c49
ludiazriod <luis.diaz@langoo.io>
2023-01-03 14:46:55 +0100

    reviews added

--- src/graphql/resolvers/Mutations.ts
+++ src/graphql/resolvers/Mutations.ts
@@ -1,14 +1,13 @@
 import {ApolloError} from "apollo-server-express";
-import { Request, Response } from "express";
 import {Db, Collection, WithId, ObjectId} from "mongodb";
-import {User, Cube, InputCube, PublicCube} from "../../mongodb/mongoTypes"
+import {User, Cube, InputCube, PublicCube, Context, Review} from "../../mongodb/mongoTypes"
 import {app} from "../../server"
 // @ts-ignore
 import { v4 as uuidv4 } from 'uuid';
 import axios from "axios";
 
 const Mutation = {
-    logIn: async(parents: any, args: {username: string, email: string | undefined, password: string}, ctx: {req: Request, res: Response}): Promise<{creator: string, authToken: string} | undefined> => {
+    logIn: async(parents: any, args: {username: string, email: string | undefined, password: string}, ctx: Context): Promise<{creator: string, authToken: string} | undefined> => {
         try{
             const db: Db = app.get("db");
             const userCollection: Collection<User> = db.collection<User>("Users")
@@ -32,7 +31,7 @@ const Mutation = {
             throw new ApolloError(`${e}`);
         }
     },
-    signUp: async(parents: any, args: {username: string, email: string, password: string}, ctx: {req: Request, res: Response}): Promise<string | undefined> => {
+    signUp: async(parents: any, args: {username: string, email: string, password: string}, ctx: Context): Promise<string | undefined> => {
         try{
             const db: Db = app.get("db");
             const userCollection: Collection<User> = db.collection<User>("Users")
@@ -44,7 +43,8 @@ const Mutation = {
                     email: args.email, 
                     password: args.password,
                     cubes: [], 
-                    authToken: token
+                    authToken: token,
+                    profileImg: ""
                 })
                 return token
             }
@@ -53,7 +53,7 @@ const Mutation = {
             throw new ApolloError(`${e}`);
         }
     },
-    addCubeCard: async(parent: any, args: {info: InputCube}, ctx:{req: Request, res: Response}): Promise<boolean> => {
+    addCubeCard: async(parent: any, args: {info: InputCube}, ctx:Context): Promise<boolean> => {
         try{
             const db: Db = app.get("db")
             const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes")
@@ -62,11 +62,15 @@ const Mutation = {
             if(!findCard){
                 const user = await userCollection.findOne({_id: new ObjectId(args.info.creator)})
                 if(user){
-                    const newCubeInfo = {
+                    const newCubeInfo: Cube = {
                         ...args.info,
                         creator: {
                             creatorId: args.info.creator,
                             username: user.username
+                        },
+                        cardReviewPoints: {
+                            reviewMean: 0,
+                            reviews: []
                         }
                     }
                     const {insertedId} = await cubeCollection.insertOne(newCubeInfo);
@@ -79,7 +83,7 @@ const Mutation = {
             throw new ApolloError(`${e}`);
         }
     },
-    logOut: async(parent: any, args: {authToken: string}, ctx:{req: Request, res: Response}): Promise<boolean> => {
+    logOut: async(parent: any, args: {authToken: string}, ctx:Context): Promise<boolean> => {
         try{
             const db: Db = app.get("db")
             const userCollection: Collection<User> = db.collection<User>("Users")
@@ -89,7 +93,7 @@ const Mutation = {
             throw new ApolloError(`${e}`);
         }
     },
-    makePublic: async(parent: any, args: {id: ObjectId}, ctx:{req: Request, res: Response}): Promise<boolean> => {
+    makePublic: async(parent: any, args: {id: ObjectId}, ctx:Context): Promise<boolean> => {
         try{
             const db: Db = app.get("db")
             const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes")
@@ -99,13 +103,15 @@ const Mutation = {
             throw new ApolloError(`${e}`);
         }
     },
-    deleteCube: async(parent: any, args: {id: ObjectId}, ctx: {req: Request, res: Response}): Promise<boolean> => {
+    deleteCube: async(parent: any, args: {id: ObjectId}, ctx: Context): Promise<boolean> => {
         try{
             const db: Db = app.get("db");
             const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes")
             const userCollection: Collection<User> = db.collection<User>("Users")
+            const reviewCollection: Collection<Review> = db.collection<Review>("Reviews")
             const cube = await cubeCollection.findOne({_id: new ObjectId(args.id)})
             if(cube){
+                await reviewCollection.deleteMany({cubeId: cube._id})
                 await userCollection.updateMany({cubes: new ObjectId(args.id)}, {$pull: {cubes: new ObjectId(args.id)}})
                 await axios.post(`${process.env.IMG_API_URL}/delete/${cube.cardImg}`)
                 await cubeCollection.deleteOne(cube)
@@ -116,7 +122,7 @@ const Mutation = {
             throw new ApolloError(`${e}`);
         }
     },
-    changeCube: async(parent: any, args: {input: Cube}, ctx: {req: Request, res: Response}): Promise<boolean> => {
+    changeCube: async(parent: any, args: {input: Cube}, ctx: Context): Promise<boolean> => {
         try{
             const db: Db = app.get("db");
             const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes")
@@ -136,19 +142,82 @@ const Mutation = {
             throw new ApolloError(`${e}`);
         }
     },
-    cloneCube: async(parent: any, args: {input: {id: ObjectId, authToken: string}}, ctx: {req: Request, res: Response}): Promise<boolean> => {
+    cloneCube: async(parent: any, args: {input: {id: ObjectId, authToken: string}}, ctx: Context): Promise<boolean> => {
         try{
             const db: Db = app.get("db");
             const userCollection: Collection<User> = db.collection<User>("Users")
             const user = await userCollection.findOne({authToken: args.input.authToken})
             if(user){
                 if(!user.cubes.find((elem) => elem === new ObjectId(args.input.id))){
-                    await userCollection.updateOne(user, {$set: {cubes: [...user.cubes, new ObjectId(args.input.id)]}})
+                    await userCollection.updateOne(user, {$push: {cubes: new ObjectId(args.input.id)}})
                     return true
                 }
                 return false
             }
             return false
+        }catch(e){
+            throw new ApolloError(`${e}`);
+        }
+    },
+    changeProfileImg: async(parent: any, args: {input: {imgName: string, authToken: string}}, ctx: Context): Promise<boolean> => {
+        try{
+            const db: Db = app.get("db");
+            const userCollection: Collection<User> = db.collection<User>("Users")
+            await userCollection.findOneAndUpdate({authToken: args.input.authToken}, {$set: {profileImg: args.input.imgName}})
+            return true
+        }catch(e){
+            throw new ApolloError(`${e}`);
+        }
+    },
+    deleteUser: async(parent: any, args: {input: {authToken: string, password: string}}, ctx: Context): Promise<boolean> => {
+        try{
+            const db: Db = app.get("db");
+            const userCollection: Collection<User> = db.collection<User>("Users")
+            const user = await userCollection.findOne({authToken: args.input.authToken, password: args.input.password})
+            if(user){
+                const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes")
+                const reviewCollection: Collection<Review> = db.collection<Review>("Reviews")
+                const userReviews = await reviewCollection.find({userId: user._id}).toArray();
+                await cubeCollection.updateMany({"cardReviewPoints.reviews": {$in: userReviews.map((elem) => elem._id)}}, {$pull: {"cardReviewPoints.reviews": userReviews.map((elem) => elem._id)}})
+                await reviewCollection.deleteMany({userId: user._id})
+                if(user.profileImg?.length !== 0){
+                    await axios.post(`${process.env.IMG_API_URL}/delete/${user.profileImg}`)
+                }
+                await cubeCollection.deleteMany({_id: {$in: user.cubes}})
+                await userCollection.updateMany(
+                    {cubes: user.cubes}, 
+                    {$pull: {cubes: user.cubes}}
+                )
+                await userCollection.deleteOne(user)
+                return true
+            }            
+            return false
+        }catch(e){
+            throw new ApolloError(`${e}`);
+        }
+    },
+    addReview: async(parent: any, args: {input: {userId: string, cubeId: string, points: number}}, ctx: Context): Promise<boolean> => {
+        try{
+            const db: Db = app.get("db");
+            const userCollection: Collection<User> = db.collection<User>("Users")
+            const user = await userCollection.findOne({_id: new ObjectId(args.input.userId)})
+            if(user){
+                const reviewCollection: Collection<Review> = db.collection<Review>("Reviews")
+                const reviewed = await reviewCollection.find({userId: new ObjectId(args.input.userId)}).toArray()
+                if(!reviewed.find((elem) => elem.cubeId === new ObjectId(args.input.cubeId))){
+                    const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes")
+                    const {insertedId} = await reviewCollection.insertOne({
+                        userId: new ObjectId(args.input.userId),
+                        cubeId: new ObjectId(args.input.cubeId),
+                        points: args.input.points
+                    })
+                    const mean = (reviewed.reduce((sum, elem) => sum + elem.points, 0)+args.input.points) / (reviewed.length + 1);
+                    await cubeCollection.findOneAndUpdate({_id: new ObjectId(args.input.cubeId)}, {$push: {"cardReviewPoints.reviews": insertedId}, $set: {"cardReviewPoints.reviewMean": mean}})
+                    return true
+                }
+                return false;
+            }
+            return false;
         }catch(e){
             throw new ApolloError(`${e}`);
         }
--- src/graphql/resolvers/Query.ts
+++ src/graphql/resolvers/Query.ts
@@ -1,11 +1,10 @@
 import {ApolloError} from "apollo-server-express";
-import { Request, Response } from "express";
 import {Db, Collection, WithId, ObjectId} from "mongodb"; 
-import {Cube, PublicCube, User} from "../../mongodb/mongoTypes"
+import {Context, Cube, ProfileCube, ProfileInfo, PublicCube, Review, User} from "../../mongodb/mongoTypes"
 import {app} from "../../server"
 
 const Query = {
-    getAllCubeCards: async(parents: any, args: {}, ctx: {req: Request, res: Response}): Promise<Cube[]> => {
+    getAllCubeCards: async(parents: any, args: {}, ctx: Context): Promise<Cube[]> => {
         try{
             const db: Db = app.get("db");
             const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes");
@@ -14,7 +13,7 @@ const Query = {
             throw new ApolloError(`${e}`);
         }
     },
-    getCubesByUser: async(parents: any, args: {authToken: string}, ctx: {req: Request, res: Response}): Promise<Cube[]> => {
+    getCubesByUser: async(parents: any, args: {authToken: string}, ctx: Context): Promise<Cube[]> => {
         try{
             const db: Db = app.get("db");
             const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes");
@@ -28,7 +27,7 @@ const Query = {
             throw new ApolloError(`${e}`);
         }
     },
-    getCubeById: async(parents: any, args: {cubeId: string}, ctx: {req: Request, res: Response}): Promise<WithId<Cube> | null> => {
+    getCubeById: async(parents: any, args: {cubeId: string}, ctx: Context): Promise<WithId<Cube> | null> => {
         try{
             const db: Db = app.get("db");
             const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes");
@@ -37,7 +36,7 @@ const Query = {
             throw new ApolloError(`${e}`);
         }
     },
-    getUser: async(parents: any, args: {authToken: string}, ctx: {req: Request, res: Response}): Promise<WithId<User> | null> => {
+    getUser: async(parents: any, args: {authToken: string}, ctx: Context): Promise<WithId<User> | null> => {
         try{
             const db: Db = app.get("db")
             const user: WithId<User> | null = await db.collection<User>("Users").findOne({authToken: args.authToken})
@@ -57,10 +56,8 @@ const Query = {
                 cardReviewPoints: number | undefined, 
                 cubeType: boolean | undefined
             } | undefined}, 
-            ctx: {
-                req: Request, 
-                res: Response
-            }): Promise<WithId<Cube>[] | null> => {
+            ctx: Context
+            ): Promise<WithId<Cube>[] | null> => {
         try{
             const db: Db = app.get("db")
             const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes");
@@ -75,20 +72,18 @@ const Query = {
                 if(args.search. cubeName){
                     toFilter.cubeName = {$regex: args.search.cubeName}
                 }
-                if(args.search.cardReviewPoints){
-                    toFilter.cardReviewPoints = args.search.cardReviewPoints
-                }
                 if(args.search.cubeType !== undefined && args.search.cubeType){
-                    return await cubeCollection.find({...toFilter, cubeModName: {$exists: args.search.cubeType, $ne: ""} , public: true}).skip((args.page-1)*20).limit(20).toArray();
+                    return await cubeCollection.find({...toFilter, cubeModName: {$exists: args.search.cubeType, $ne: ""}, public: true, "cardReviewPoints.reviewMean": args.search.cardReviewPoints ? args.search.cardReviewPoints : 0}).skip((args.page-1)*20).limit(20).toArray();
                 }
-                return await cubeCollection.find({...toFilter, public: true}).skip((args.page-1)*20).limit(20).toArray(); 
+                console.log(args.search.cardReviewPoints ? args.search.cardReviewPoints : 0)
+                return await cubeCollection.find({...toFilter, "cardReviewPoints.reviewMean": args.search.cardReviewPoints ? args.search.cardReviewPoints : 0, public: true}).skip((args.page-1)*20).limit(20).toArray(); 
             }
             return await cubeCollection.find({public: true}).skip((args.page-1)*20).limit(20).toArray(); 
         }catch(e){
             throw new ApolloError(`${e}`);
         }
     },
-    getCreator: async(parents: any, args: {id: ObjectId}, ctx: {req: Request, res: Response}): Promise<string> => {
+    getCreator: async(parents: any, args: {id: ObjectId}, ctx: Context): Promise<string> => {
         try{
             const db: Db = app.get("db")
             const user: WithId<User> | null = await db.collection<User>("Users").findOne({_id: new ObjectId(args.id)})
@@ -97,6 +92,93 @@ const Query = {
             throw new ApolloError(`${e}`);
         }
     },
+    getProfileInfo: async(parents: any, args: {input: {authToken?: string, id?: string}}, ctx: Context): Promise<ProfileInfo | undefined> => {
+        try{
+            const db: Db = app.get("db")
+            const userCollection = db.collection<{profileImg: string, cubes: ObjectId[], username: string}>("Users")
+            const user: WithId<{profileImg: string, cubes: ObjectId[], username: string}> | null = args.input.authToken ? await userCollection.findOne({authToken: args.input.authToken}) : await userCollection.findOne({_id: new ObjectId(args.input.id)})
+            if(user){
+                const profileUser = await userCollection.findOne({_id: new ObjectId(args.input.id)})
+                const cubeCollection: Collection<Cube> = db.collection<Cube>("Cubes");
+                const reviewCollection: Collection<Review> = db.collection<Review>("Reviews")
+                const cubes = await cubeCollection.find({"creator.creatorId": args.input.id, public: true}).toArray()
+                const numReviews = (await reviewCollection.find({userId: user._id}).toArray()).length;
+                let mean = 0;
+                let reviewsNum = 0;
+                cubes.forEach((elem) => {
+                    if(elem.cardReviewPoints.reviews.length > 0){
+                        mean += elem.cardReviewPoints.reviewMean,
+                        reviewsNum += elem.cardReviewPoints.reviews.length 
+                    }
+                })
+                mean = mean / cubes.filter((elem) => elem.cardReviewPoints.reviews.length > 0).length;
+                
+                return {
+                    username: profileUser ? profileUser.username : "", 
+                    profileImg: user.profileImg,
+                    numReviews: numReviews,
+                    cardReviews: {
+                        cardsTotalMean: mean ? Math.round(mean) : 0,
+                        cardsTotalReviews: reviewsNum
+                    },
+                    cubes: cubes.map((elem) => {
+                        return {
+                            _id: elem._id,
+                            cubeName: elem.cubeName,
+                            cubeDimensions: elem.cubeDimensions,
+                            cubeModName: elem.cubeModName ? elem.cubeModName : "",
+                            cubeModel: elem.cubeModel ? elem.cubeModel : "",
+                            cubeBrand: elem.cubeBrand ? elem.cubeBrand : "",
+                            cubeDesigner: elem.cubeDesigner ? elem.cubeDesigner : "",
+                            cardMainTitle: elem.cardMainTitle,
+                            cardText: elem.cardText,
+                            cardReviewPoints: elem.cardReviewPoints,
+                            cardImg: elem.cardImg
+                        }
+                    })
+                }
+            }
+            return undefined
+        }catch(e){
+            throw new ApolloError(`${e}`)
+        }
+    },
+    isUser: async(parents: any, args: {input: {authToken: string, id: string}}, ctx: Context): Promise<boolean> => {
+        try{
+            const db: Db = app.get("db")
+            const userCollection = db.collection<{profileImg: string, cubes: ObjectId[]}>("Users")
+            const user = await userCollection.findOne({_id: new ObjectId(args.input.id),authToken: args.input.authToken})
+            if(user){
+                return true
+            }
+            return false
+        }catch(e){
+            throw new ApolloError(`${e}`)
+        }
+    },
+    getReview: async(parents: any, args: {input: {authToken: string, cubeId: string}}, ctx: Context): Promise<{reviewed: boolean, reviewValue: number}> => {
+        try{
+            const db: Db = app.get("db")
+            const userCollection = db.collection<{profileImg: string, cubes: ObjectId[]}>("Users")
+            const user = await userCollection.findOne({authToken: args.input.authToken})
+            if(user){
+                const review = await db.collection<Review>("Reviews").findOne({userId: user._id, cubeId: new ObjectId(args.input.cubeId)})
+                return review ? {
+                    reviewed: true,
+                    reviewValue: review.points
+                } : {
+                    reviewed: false,
+                    reviewValue: 0
+                }
+            }
+            return {
+                reviewed: false,
+                reviewValue: 0
+            }
+        }catch(e){
+            throw new ApolloError(`${e}`)
+        }
+    }
 }
 
 export default Query;
